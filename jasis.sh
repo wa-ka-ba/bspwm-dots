16c62480d6786f4195d812d77d78fd711cabbd9a
wa-ka-ba <wkb.wakaba@tuta.io>
2023-06-03 08:14:23 +0800

    dumbass forgot an important package

--- jasis.sh
+++ jasis.sh
@@ -33,7 +33,7 @@ cd
 
 # Install packages from the arch repo
 echo "Installing packages from the arch repo..."
-sudo pacman -S bspwm nitrogen dunst kitty neofetch polybar rofi sxhkd playerctl geany lxappearance-gtk3 nemo ttf-hanazono fcitx-im fcitx-configtool fcitx-mozc feh arandr network-manager-applet lxsession-gtk3 notification-daemon stalonetray gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb
+sudo pacman -S bspwm nitrogen dunst xorg-xsetroot kitty neofetch polybar rofi sxhkd playerctl geany lxappearance-gtk3 nemo ttf-hanazono fcitx-im fcitx-configtool fcitx-mozc feh arandr network-manager-applet lxsession-gtk3 notification-daemon stalonetray gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb
 sleep 2
 echo "Installing packages from the aur..."
 yay -S zscroll ttf-material-design-iconic-font nerd-fonts-jetbrains-mono-160 ttf-meslo-nerd betterlockscreen picom-pijulius-git papirus-folders-catppuccin-git neo-matrix
