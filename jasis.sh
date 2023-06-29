4ec35eb09a8448d487a243591da3b016a9e15fff
wa-ka-ba <wkb.wakaba@tuta.io>
2023-06-30 00:25:52 +0800

    minor changes

--- jasis.sh
+++ jasis.sh
@@ -33,24 +33,29 @@ cd
 
 # Install packages from the arch repo
 echo "Installing packages from the arch repo..."
+sleep 3
 sudo pacman -S bspwm nitrogen dunst xorg-xsetroot kitty neofetch polybar rofi sxhkd playerctl geany lxappearance-gtk3 nemo ttf-hanazono fcitx-im fcitx-configtool fcitx-mozc feh arandr network-manager-applet lxsession-gtk3 notification-daemon stalonetray gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb
 sleep 2
 echo "Installing packages from the aur..."
+sleep 3
 yay -S zscroll ttf-material-design-iconic-font nerd-fonts-jetbrains-mono-160 ttf-meslo-nerd betterlockscreen picom-pijulius-git papirus-folders-catppuccin-git neo-matrix
 sleep 2
 cd
 
 # Install papirus folder
 echo "Installing folder theme..."
+sleep 3
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 sleep 2
 echo "Applying catppuccin folder theme..."
+sleep 3
 yay -S papirus-folders-catppuccin-git
 papirus-folders -C cat-frappe-pink 
 sleep 2
 
 # Generate locales for Japanese text
 echo "Generating locales for Japanese text..."
+sleep 3
 cd
 cd bspwm-dots
 sudo mv locale.gen /etc/locale.gen 
